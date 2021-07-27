442791fc329b34988b9812795255aea413653d14
Susmita <susa.limbu@gmail.com>
2021-07-27 22:27:29 +0545

    user table added

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,10 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('blood')->nullable();
+            $table->string('location')->nullable();
+            $table->string('mobile')->nullable();
+            $table->integer('is_admin')->default(1);
             $table->rememberToken();
             $table->timestamps();
         });
