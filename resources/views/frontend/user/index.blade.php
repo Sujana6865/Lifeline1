e52f9e768bfbda53feb77d1fbe59d45a8cc0a8e0
Susmita <susa.limbu@gmail.com>
2021-07-29 13:56:21 +0545

    userview created

--- resources/views/frontend/user/index.blade.php
+++ resources/views/frontend/user/index.blade.php
@@ -7,24 +7,16 @@
         Here are the list of nearby donors
     </p>
 
-    
-   <div class="row mt-sm-5 border">
-        <div class="col-10 float-left">
-            <h1>Name</h1>
-        </div>
-        <div class="col-sm-2 mt-sm-3 float-right">
-            <a href="" style="text-decoration: none;" >View Details</a>
-        </div>
-   </div>
-
-   <div class="row border mt-sm-3">
-    <div class="col-10 float-left">
-        <h1>Name</h1>
-    </div>
-    <div class="col-sm-2 mt-sm-3 float-right">
-        <a href=""style="text-decoration: none;" >View Details</a>
-    </div>
-</div>
+    @foreach ($users as $user)
+    <div class="row mt-sm-5 border">
+         <div class="col-10 float-left">
+             <h1>{{ $user->name }}</h1>
+         </div>
+         <div class="col-sm-2 mt-sm-3 float-right">
+             <a href="/user/{{ $user->id }}" style="text-decoration: none;" >View Details</a>
+         </div>
+    </div> 
+    @endforeach
 
     
 
