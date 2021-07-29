074640e687c0a8c3a09b9d0d06032eb054e7bf67
Sujana6865 <Sujanalimbu6865@gmail.com>
2021-07-29 13:45:48 +0545

    user information created

---
+++ resources/views/frontend/user/show.blade.php
@@ -0,0 +1,13 @@
+@extends('frontend.template.app')
+
+@section('content')
+    <div>
+        <h3>User Information</h3>
+        <address>
+            User:{{ $user->name }} <br>
+            Location:{{ $user->location }} <br>
+            Blood Group: {{ $user->blood }} <br>
+            Phone No.:{{ $user->mobile }}
+        </address>
+    </div>
+@endsection
