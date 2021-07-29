ba980be6fb9bdfcc950b226a863b7894769a73ff
Susmita <susa.limbu@gmail.com>
2021-07-29 14:00:05 +0545

    blood group added to search result

--- resources/views/frontend/user/index.blade.php
+++ resources/views/frontend/user/index.blade.php
@@ -10,7 +10,8 @@
     @foreach ($users as $user)
     <div class="row mt-sm-5 border">
          <div class="col-10 float-left">
-             <h1>{{ $user->name }}</h1>
+             <h1>{{ $user->name }}</h1><br>
+             <h1>{{ $user->blood }}</h1>
          </div>
          <div class="col-sm-2 mt-sm-3 float-right">
              <a href="/user/{{ $user->id }}" style="text-decoration: none;" >View Details</a>
